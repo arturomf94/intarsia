444a79a1a03b9e420f02be08fee2aca604798bea
Arturo Marquez <arturomf94@gmail.com>
2022-05-01 21:30:35 -0500

    Add reading and transformation

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ extern crate image;
 mod err;
 use err::Error;
 use image::io::Reader as ImageReader;
+use image::DynamicImage;
 use std::process;
 use std::{fs, path::PathBuf};
+extern crate strum_macros;
+use strum_macros::EnumString;
 
 const WELCOME_MSG: &str = "
 '||' '|.   '|' |''||''|     |     '||''|.    .|'''.|  '||'     |     
@@ -17,17 +20,46 @@ const WELCOME_MSG: &str = "
 .||. .|.   '|    .||.   .|.  .||. .||.  '|' |'....|'  .||. .|.  .||. 
 ";
 
+#[derive(EnumString)]
+enum ImageType {
+    #[strum(serialize = "original")]
+    Original,
+    #[strum(serialize = "processed")]
+    Processed,
+}
+
+struct Image {
+    image_type: ImageType,
+    path: PathBuf,
+    image: DynamicImage,
+}
+
+struct Instructions {
+    /// This is the text with the instructions.
+    /// TODO: Add functionality so that these instructions can
+    /// be read from a given point within the project.
+    text: String,
+}
+
+/// Represents a project instance. This holds information about
+/// its name, where its data is stored, and the images
 struct Project {
     /// The name of this project.
     name: String,
-    // The path to the project files.
+    /// The path to the project files.
     path: PathBuf,
+    /// Original image.
+    original_image: Option<Image>,
+    /// Processed image.
+    processed_image: Option<Image>,
+    /// The instructions for this crochet project.
+    instructions: Option<Instructions>,
 }
 
 impl Project {
     /// Creates a new instance of a project, with a given name
     /// and path.
-    fn new(name: String) -> Result<Project, Error> {
+    fn new(name: &String) -> Result<Project, Error> {
         // Determine the home directory.
         let mut path: PathBuf;
         match dirs::home_dir() {
@@ -48,7 +80,55 @@ impl Project {
             return Err(Error::ExistsAlready.into());
         }
         fs::create_dir(path.as_path()).map_err(|e| Error::External(e.to_string()))?;
-        Ok(Project { name, path })
+        Ok(Project {
+            name: name.to_string(),
+            path,
+            original_image: None,
+            processed_image: None,
+            instructions: None,
+        })
+    }
+
+    /// This function removes the current project, if it indeed
+    /// exists already.
+    fn remove_project(&self) {
+        if self.path.exists() {
+            fs::remove_dir_all(self.path.as_path()).unwrap();
+        }
+    }
+
+    fn read_image(&mut self, image: String) -> Result<(), Error> {
+        let image = ImageReader::open(&image)
+            .map_err(|e| Error::External(e.to_string()))?
+            .decode()
+            .map_err(|e| Error::External(e.to_string()))?;
+        let mut path: PathBuf = self.path.clone();
+        path.push("original.jpg");
+        image
+            .save(&path)
+            .map_err(|e| Error::External(e.to_string()))?;
+        self.original_image = Some(Image {
+            image_type: ImageType::Original,
+            path,
+            image,
+        });
+        Ok(())
+    }
+
+    fn transform_image(&mut self) -> Result<(), Error> {
+        let mut image = self.original_image.as_ref().unwrap().image.clone();
+        image = image.flipv();
+        let mut path: PathBuf = self.path.clone();
+        path.push("processed.jpg");
+        image
+            .save(&path)
+            .map_err(|e| Error::External(e.to_string()))?;
+        self.processed_image = Some(Image {
+            image_type: ImageType::Processed,
+            path,
+            image,
+        });
+        Ok(())
     }
 }
 
@@ -63,6 +143,18 @@ enum SubCommand {
         #[structopt(short, long)]
         image: String,
     },
+    ///
+    Remove {
+        name: String,
+    },
+    Show {
+        name: String,
+        #[structopt(short, long)]
+        r#type: String,
+    },
+    Instructions {
+        name: String,
+    },
 }
 
 #[derive(StructOpt, Debug)]
@@ -72,68 +164,42 @@ struct Intarsia {
     cmd: SubCommand,
 }
 
-/// Creates a new intarsia project, with a given name.
-fn create_new_project(name: &String, image: String) -> Result<(), Error> {
-    let mut project_path_buf: PathBuf;
-    match dirs::home_dir() {
-        Some(ppb) => {
-            project_path_buf = ppb;
-        }
-        None => {
-            return Err(Error::External("Could not determine home dir".to_string()));
-        }
-    }
-    project_path_buf.push(".intarsia/");
-    if !project_path_buf.as_path().exists() {
-        fs::create_dir(project_path_buf.as_path()).map_err(|e| Error::External(e.to_string()))?;
-    }
-    project_path_buf.push(format!("{}/", name));
-    // Throw error if path already exists
-    if project_path_buf.as_path().exists() {
-        return Err(Error::ExistsAlready.into());
-    }
-    fs::create_dir(project_path_buf.as_path()).map_err(|e| Error::External(e.to_string()))?;
-    let _img = ImageReader::open(&image)
-        .map_err(|e| Error::External(e.to_string()))?
-        .decode()
-        .map_err(|e| Error::External(e.to_string()))?;
-    println!(
-        "Successfully created new project {}, with the image file `{}`",
-        name, image
-    );
-    Ok(())
-}
-
-fn clean_up_project(name: String) -> Result<(), Error> {
-    println!("Remove {}", name);
-    // fs::remove_dir_all(&template_dir_buf.as_path())
-    Ok(())
-}
-
-fn main() -> Result<(), Error> {
+fn main() {
     // Run subcommand
     match Intarsia::from_args().cmd {
         SubCommand::New { name, image } => {
-            let project = Project::new(name);
+            let project = Project::new(&name);
             match project {
-                // If creation failed because the project exists
-                // already, then return the error, but do not
-                // clean-up the environment.
-                Err(Error::ExistsAlready) => {
-                    eprintln!("{}", Error::ExistsAlready);
-                    return Err(Error::ExistsAlready);
-                    // process::exit(1);
-                }
                 Err(err) => {
-                    eprintln!("{}", err);
-                    if let Err(err) = clean_up_project(name) {
-                        eprintln!("{}", err);
-                        process::exit(1);
-                    };
+                    eprintln!("Could not create new project. Error: {}", err);
                     process::exit(1);
                 }
-                Ok(_) => Ok(()),
+                Ok(mut project) => {
+                    match project.read_image(image) {
+                        Err(err) => {
+                            eprintln!("Could not read image. Error: {}", err);
+                            project.remove_project();
+                            process::exit(1);
+                        }
+                        _ => (),
+                    }
+                    match project.transform_image() {
+                        Err(err) => {
+                            eprintln!("Could not transform image. Error: {}", err);
+                            project.remove_project();
+                            process::exit(1);
+                        }
+                        _ => (),
+                    }
+                    println!(
+                        "Succsessfully created project {}, stored at {:?}",
+                        project.name, project.path
+                    );
+                }
             }
+        }
+        _ => {
+            println!("Not implemented.")
         }
     }
 }
